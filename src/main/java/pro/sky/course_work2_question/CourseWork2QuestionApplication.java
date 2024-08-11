c49f6ab673a4f912d23f93e7c672e5860691d4bc
Chowo <bob-xiii@rambler.ru>
2024-08-11 19:46:02 +0200

    Initial commit

---
+++ src/main/java/pro/sky/course_work2_question/CourseWork2QuestionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work2_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2QuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2QuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/course_work2_question/CourseWork2QuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work2_question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2QuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
